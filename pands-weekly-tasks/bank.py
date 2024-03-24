569db0472b1ea1c41e0f8e29647d845351635eef
unknown <ellenmcgrory1@gmail.com>
2024-03-24 21:03:27 +0000

    updated bank.py python program

--- pands-weekly-tasks/bank.py
+++ pands-weekly-tasks/bank.py
@@ -5,13 +5,26 @@
 
 # Author Ellen McGrory
 
+# The result can be stored as a float, but due to the way floats are stored
+# floats are not a good way to store currency. While it may not cause an error (imprecision error)
+# in the below format. When multiple addition, subtratation, etc operations are performed on the 
+# currency value then these floats (or floating point numbers) imprecision can be added to the currency
+# due to rounding errors. 
+# For this reason, total amount will be returned as a decimal rather than a float using the decmimal package
+
+# Background reading on floating point numbers and decimal module
+# URL: https://husobee.github.io/money/float/2016/09/23/never-use-floats-for-currency.html
+# URL: https://docs.python.org/3/library/decimal.html
+
+from decimal import *
+
 # both inputs are converted to intergers so that they can be added
-amount1 = int(input("Enter amount1(in cent):")) 
-amount2 = int(input("Enter amount2(in cent):"))
+amount1 = int(input("Enter amount1(in cent): ")) 
+amount2 = int(input("Enter amount2(in cent): "))
 
 
 # dividing by 100 to convert cent amount to Euro amount
-total_amount = (amount1 + amount2)/100
+total_amount = Decimal((amount1 + amount2)/100)
 
 # .2f indicates that rounding is done to two decimal places
 # adding the euro sign using unicode character name
